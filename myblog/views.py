cf69f427154966a04e1e965c337505f09b02e2d6
Abraham <abrahammekuria73@gmail.com>
2024-07-04 14:09:45 +0300

    second commit but some error

--- myblog/views.py
+++ myblog/views.py
@@ -1,8 +1,17 @@
 from django.views.generic import ListView, DetailView # new
+from django.views.generic.edit import CreateView
+
 from .models import Post
+
 class BlogListView(ListView):
     model = Post
     template_name = "home.html"
+
 class BlogDetailView(DetailView): # new
     model = Post
     template_name = "post_detail.html"
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = "post_new.html"
+    fields = ["title", "author", "body"]
