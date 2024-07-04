11c0a9705a76a337ad6744410e2e6d2981a60d88
Abraham <abrahammekuria73@gmail.com>
2024-07-04 16:53:16 +0300

    error solved

--- myblog/models.py
+++ myblog/models.py
@@ -10,7 +10,7 @@ class Post(models.Model):
         on_delete=models.CASCADE,
     )
     body = models.TextField()
-def __str__(self):
-    return self.title
-def get_absolute_url(self):
-    return reverse("post_detail", kwargs={"pk": self.pk})
+    def get_absolute_url(self):
+        return reverse("post_detail", kwargs={"pk": self.pk})
+    def __str__(self):
+        return self.title
